93c69960817672470a61dbd67c40663eab84b94b
Gustavo Henrique <gustavohos5105@gmail.com>
2025-11-18 01:46:24 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/editor/settings/DesignSettings.tsx
+++ src/components/editor/settings/DesignSettings.tsx
@@ -1,5 +1,7 @@
+
 'use client';
 
+import * as React from 'react';
 import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
