6c5051c16bd7fd8f0bf49816f1471426e6211982
Gustavo Henrique <gustavohos5105@gmail.com>
2025-11-26 04:03:18 +0000

    Os caracteres de padrão tem que vir preto

--- src/components/editor/canvas/TextoCanvasComponent.tsx
+++ src/components/editor/canvas/TextoCanvasComponent.tsx
@@ -3,11 +3,11 @@
 import type { CanvasComponentData } from '../types';
 
 export const TextoCanvasComponent = ({ component }: { component: CanvasComponentData }) => {
-  const { content = '' } = component.props;
+  const { content = '<h3><b>Título</b></h3><p>Preencha o texto.</p>' } = component.props;
 
   return (
     <div
-      className="w-full max-w-none text-foreground"
+      className="w-full max-w-none text-black"
       dangerouslySetInnerHTML={{ __html: content }}
     />
   );
